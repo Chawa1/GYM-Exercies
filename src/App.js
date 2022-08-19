75edca3c39922c43cde105fa5f2d3b4a30d5b6ca
Chawa1 <chawankhalil11@gmail.com>
2022-08-19 23:39:01 +0300

    add pages and components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
 
 function App() {
   return (
-    <div>hello</div>
+    <Box width='400px'>
+<Navbar/>
+<Routes>
+  <Route path="/" element={<Home/>} />
+  <Route path="/exercise/:id" element={<ExerciseDetail/>} />
+</Routes>
+    </Box>
   );
 }
 
